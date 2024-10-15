dd9dbd7acf14d6460c6fde2c409be4ca1fd45a0e
matthewm09 <matthewm6775@nycstudents.net>
2024-10-15 08:48:12 -0400

    Adding movement
    We gave the keys different values

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -18,22 +18,22 @@ public class PlayerController : MonoBehaviour
         float horizontal = 0.0f;
         if(Keyboard.current.rightArrowKey.isPressed)
         {
-            horizontal = 1.0f;
+            horizontal = .50f;
         }
         else if(Keyboard.current.leftArrowKey.isPressed)
         {
-            horizontal = -1.0f;
+            horizontal = -.50f;
         } 
         Debug.Log(horizontal);
         
         float vertical = 0.0f;
         if(Keyboard.current.downArrowKey.isPressed)
         {
-            vertical = -1.0f;
+            vertical = -.50f;
         }
         else if(Keyboard.current.upArrowKey.isPressed)
         {
-            vertical = 1.0f;
+            vertical = .50f;
         }
         Debug.Log(vertical);
 
